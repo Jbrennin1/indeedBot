f98b14c1f4607131b99398c0e261b7fa69d6b9a6
Brennin <bkjoiner1@gmail.com>
2024-04-04 00:02:17 -0400

    working application

--- libs/funcs.js
+++ libs/funcs.js
@@ -3,20 +3,125 @@ import { queryString } from '../index.js'
 
 let totalJobs = 0
 let pageCount = 0
-async function determinePageType(browser, jobs, page, count)
-    {
-            await page.bringToFront()
-            let title = await page.title()
-            console.log(count + "|" + title)
-            if (await page.$('#input-firstName') !== null) { // Checks if the first name field is present
-                await fillContactInformationForm(page);
-                return; // Stop execution to prevent timeout
-            }
+async function determinePageType(browser, jobs, page, count) {
+    console.log("Determining page type...");
+    await page.bringToFront();
 
-            setTimeout(async() => {
-                await evaluateInputs(browser, jobs, page, count, title)
-            }, 7000);
+    const maxAttempts = 10;
+    let attempts = 0;
+
+    while (attempts < maxAttempts) {
+        attempts++;
+        console.log(`Attempt ${attempts} to determine page...`);
+
+        const questionnaireHeader = await page.$('.ia-BasePage-heading');
+        if (questionnaireHeader) {
+            const headerText = await page.evaluate(header => header.textContent, questionnaireHeader);
+            if (headerText.includes('Answer these questions from the employer')) {
+                console.log("Questionnaire page detected.");
+                await fillOutQualificationQuestions(page, browser, jobs, count);
+                return;
+            }
+        }
+
+        if (await page.$('#input-firstName') !== null) {
+            console.log("Contact information form detected.");
+            await fillContactInformationForm(page, browser, jobs, count);
+            return;
+        }
+
+        if (await page.$('#input-addressLine') !== null) {
+            console.log("Location details form detected.");
+            await fillLocationDetailsForm(page, browser, jobs, count);
+            return;
+        }
+
+        if (await page.$("input[type='radio'][value='SAVED_FILE_RESUME']") !== null) {
+            console.log("Resume selection page detected.");
+            await selectUploadedResume(page, browser, jobs, count);
+            return;
+        }
+
+        const submitApplicationButton = await page.$x("//button[contains(., 'Submit your application')]");
+        const qualificationsHeading = await page.$('h1.ia-BasePage-heading');
+        const continueButton = await page.$('.ia-QualificationIntervention-continue');
+
+        if (submitApplicationButton.length > 0) {
+            console.log('Application review page detected.');
+            await page.waitForTimeout(2000);
+
+            await submitApplicationButton[0].click();
+            console.log('Clicked "Submit your application" button.');
+            await page.waitForTimeout(2000);
+            return;
+        }
+
+        if (qualificationsHeading) {
+            const headingText = await page.evaluate(element => element.textContent, qualificationsHeading);
+            if (headingText.includes('These qualifications may be important for this job')) {
+                console.log('Qualifications page detected.');
+                if (continueButton) {
+                    await continueButton.click();
+                    console.log('Clicked "Continue applying" button.');
+                    await page.waitForTimeout(2000);
+                    return await determinePageType(browser, jobs, page, count);
+                } else {
+                    console.log('Continue applying button not found on qualifications page.');
+                }
+            } else {
+                console.log('Unknown page detected.');
+            }
+        }
+
+        const hasInputs = await page.$eval('input', input => input !== null);
+        const hasContinueButton = await page.$eval('button[type="button"]', button => button !== null);
+
+        if (hasInputs && hasContinueButton) {
+            console.log("Input page with a continue button detected.");
+            await handleInputPage(browser, jobs, page, count);
+            return;
+        } else {
+            console.log("Not an input page with a continue button.");
+
+            await page.close();
+        }
+
+        if (continueButton) {
+            console.log('Continue applying button detected.');
+            await continueButton.click();
+            console.log('Clicked "Continue applying" button.');
+            await page.waitForTimeout(2000);
+            return await determinePageType(browser, jobs, page, count);
+        }
+
+        const submissionConfirmationSelector = 'Your selector for submission confirmation';
+        if (await page.$(submissionConfirmationSelector) !== null) {
+            console.log("Application submitted successfully.");
+            return;
+        }
+
+        await page.waitForTimeout(2000);
     }
+
+    console.log("Form or confirmation not detected after maximum attempts.");
+    await page.close();
+    await clickApply(browser, jobs, count + 1);
+
+}
+
+async function handleInputPage(browser, jobs, page, count) {
+    const inputs = await page.$$('input');
+    for (const input of inputs) {
+        const inputType = await input.evaluate(node => node.type);
+        console.log("Detected input type:", inputType);
+    }
+
+    const continueButton = await page.$('button[type="button"]');
+    await continueButton.click();
+
+    await determinePageType(browser, jobs, page, count);
+
+}
 
     async function fillInputs(inputs, page)
     {
@@ -24,7 +129,6 @@ async function determinePageType(browser, jobs, page, count)
             {
             if(input != null)
             {
-                //console.log(input)
                 if(input.type == 'number')
                 {
                     for(let i = 0; i < 4; i++)
@@ -68,29 +172,113 @@ async function determinePageType(browser, jobs, page, count)
         }
     }
 
-    async function fillContactInformationForm(page) {
+    async function clearAndType(selector, value, page) {
+        console.log("typing")
+        await page.click(selector, { clickCount: 3 });
+        await page.keyboard.press('Backspace');
+        await page.type(selector, value);
+    }
+
+
+    async function fillContactInformationForm(page, browser, jobs, count) {
         console.log("Filling in the contact information form...");
 
-        // Assuming default values for demonstration. Replace with actual values or use environment variables as needed.
         const firstName = 'John';
         const lastName = 'Doe';
-        const email = 'john.doe@example.com'; // This field might be pre-filled and read-only.
-        const phoneNumber = '234567890'; // Optional, based on your requirements.
+        const phoneNumber = '3524410601';
 
-        // Fill in the First Name and Last Name
-        await page.type('#input-firstName', firstName);
-        await page.type('#input-lastName', lastName);
+        await clearAndType('#input-firstName', firstName, page);
 
-        // Check if the Phone Number field exists and fill it in (if applicable).
+        await clearAndType('#input-lastName', lastName, page);
+
         if (await page.$('#input-phoneNumber') !== null) {
-            await page.type('#input-phoneNumber', phoneNumber);
+            await clearAndType('#input-phoneNumber', phoneNumber, page);
         }
 
-        // Click the "Continue" button to proceed with the application
-        const continueButtonSelector = '.css-pqhqxt button[type="button"]'; // Adjust selector as needed
-        await page.click(continueButtonSelector);
+        await clickContinueButton(page);
 
         console.log("Contact information form submitted.");
+        await determinePageType(browser, jobs, page, count);
+    }
+
+    async function fillLocationDetailsForm(page, browser, jobs, count) {
+        console.log("Filling in the location details form...");
+
+        const streetAddress = '123 Main St';
+        const cityState = 'Orlando, FL';
+        const postalCode = '32801';
+
+        await clearAndType('#input-addressLine', streetAddress, page);
+        await clearAndType('#input-city', cityState, page);
+        await clearAndType('#input-postalCode', postalCode, page);
+
+        console.log("Location details form filled.");
+        await clickContinueButton(page);
+        await determinePageType(browser, jobs, page, count);
+    }
+
+    async function clickContinueButton(page) {
+        try {
+            await page.waitForSelector('.ia-BasePage-footer .css-pqhqxt button[type="button"]', { timeout: 5000 });
+
+            await page.click('.ia-BasePage-footer .css-pqhqxt button[type="button"]');
+            console.log("Clicked the 'Continue' button.");
+
+            await page.waitForTimeout(2000);
+
+        } catch (error) {
+            console.error("Error clicking 'Continue' button:", error.message);
+        }
+    }
+
+    async function selectUploadedResume(page, browser, jobs, count) {
+        console.log("Selecting uploaded resume...");
+
+        try {
+            const label = await page.$("label[data-testid='FileResumeCard-label']");
+
+            if (label) {
+                await label.click();
+                console.log("Uploaded resume selected.");
+            } else {
+                console.error("Uploaded resume label not found.");
+            }
+
+            await clickContinueButton(page);
+
+            await determinePageType(browser, jobs, page, count);
+        } catch (error) {
+            console.error("Error selecting uploaded resume:", error.message);
+        }
+    }
+
+    async function fillOutQualificationQuestions(page, browser, jobs, count) {
+        console.log("Filling out qualification questions...");
+
+        try {
+            const questionItems = await page.$$('.ia-Questions-item');
+
+            for (const questionItem of questionItems) {
+                const questionText = await questionItem.$eval('.css-12axhzd', node => node.textContent.trim());
+                console.log("Question:", questionText);
+
+                const radioInputs = await questionItem.$$('input[type="radio"]');
+
+                if (radioInputs.length > 0) {
+                    await radioInputs[0].click();
+                    console.log("Answered:", radioInputs[0].value);
+                } else {
+                    console.log("No radio inputs found for this question.");
+                }
+            }
+
+            await clickContinueButton(page);
+
+            await determinePageType(browser, jobs, page, count);
+
+        } catch (error) {
+            console.error("Error filling out qualification questions:", error.message);
+        }
     }
 
     async function evaluateInputs(browser, jobs, page, count, title)
@@ -150,7 +338,6 @@ async function determinePageType(browser, jobs, page, count)
                             return e != null
                         })
 
-                        // determined issue is raw value being returned, still have to debug y
                         return filteredInputs
                         })
 
@@ -159,6 +346,10 @@ async function determinePageType(browser, jobs, page, count)
                         let button = await page.waitForSelector('.ia-continueButton')
                         await button.click()
                         break
+                }
+                case 'Review your location details from your profile | Indeed.com': {
+                    await fillLocationDetailsForm(page);
+                    break;
                 }
             case 'Upload or build a resume for this application | Indeed.com' : {
                 let button = await page.waitForSelector('.ia-continueButton')
@@ -226,18 +417,14 @@ async function determinePageType(browser, jobs, page, count)
             console.log(`Navigated to job page: ${jobs[count].title}`);
 
             try {
-                // Updated selector to target the new "Apply now" button structure
                 const applyButtonSelector = 'button[id="indeedApplyButton"]';
 
-                // Wait for the "Apply now" button to be rendered in the DOM
                 await jobPage.waitForSelector(applyButtonSelector, { timeout: 5000 });
                 console.log('Found "Apply now" button.');
 
-                // Click the "Apply now" button
                 await jobPage.click(applyButtonSelector);
                 console.log('Clicked "Apply now" button.');
 
-                // Wait for navigation to ensure the click has led to a new page
                 await jobPage.waitForNavigation({ waitUntil: 'networkidle2', timeout: 10000 }).catch(e => console.log('Navigation timeout after click.'));
                 console.log('Navigation after click confirmed.');
 
@@ -247,7 +434,7 @@ async function determinePageType(browser, jobs, page, count)
             } catch (error) {
                 console.error(`Error clicking "Apply now" button for ${jobs[count].title}: ${error.message}`);
                 await jobPage.close();
-                await clickApply(browser, jobs, count + 1); // Proceed to the next job
+                await clickApply(browser, jobs, count + 1);
             }
         } else {
             let pages = await browser.pages();
@@ -314,7 +501,6 @@ async function findJobs(page, browser, count) {
 
         console.log("Jobs found: ", jobs);
 
-        // Proceed with job application logic
         if (jobs.length > 0) {
             console.log("Proceeding to apply to jobs...");
             return await clickApply(browser, jobs, count);
